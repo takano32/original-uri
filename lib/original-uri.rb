c3d763b1e17b5db65a8281bbd3bfe7cf8afd8edc
TAKANO Mitsuhiro <takano32@gmail.com>
2018-10-23 23:36:56 +0900

    fix Regex for Abema TV

--- lib/original-uri.rb
+++ lib/original-uri.rb
@@ -40,7 +40,7 @@ module OriginalURI
       self.chase_smartnews_url url
     when %r!^https?://lin\.ee/[a-zA-Z0-9]+.*!
       self.chase_line_news_url url
-    when %r!^https?://gxyt4\.app\.goo\.gl/[a-zA-Z0-9]+$!
+    when %r!^https?://gxyt4\.app\.goo\.gl[/\?].*$!
       self.chase_abema_tv_url url
     when %r!^https?://gunosy\.com/articles/.*!
       self.chase_gunosy_url url
